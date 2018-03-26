f5d8f795c611aa7e56f6358f96ac2d15039e585d
dead05 <dpant1998@gmail.com>
2018-03-26 20:14:19 +0530

    Update twit.py

--- twit.py
+++ twit.py
@@ -2,10 +2,10 @@ import tweepy
 import csv
 import pandas as pd
 ####input your credentials here
-consumer_key = 'D5swSkP3WuOA5lne0SJ1Bs32B'
-consumer_secret = 'GDzkrkE1uiyIvxMueyrE1TNUY8Qfpkv2iY7fj6BmfAV5QDFZdT'
-access_token = '744521717972877312-Bu47q61qP38f72cIWmTqwSPj6DTXZp0'
-access_token_secret = '2vpaA7PxT8O9jMH4U3Ths6JtUaPjCBCYevnPaWoSOZnay'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
